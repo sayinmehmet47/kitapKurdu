dfe7e3a7bffcf59880d9ecec3caaffc56050815f
sayin47 <sayinmehmet47@gmail.com>
2021-10-11 13:44:40 +0300

    links updated

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -29,7 +29,7 @@ export const Search = () => {
           d.map((e) => {
             return fetch(
               encodeURI(
-                `https://cloud-api.yandex.net/v1/disk/public/resources/download?public_key=https://disk.yandex.com.tr/d/sLURXWsHH4gDmwQ&path=${e.path}`
+                `https://cloud-api.yandex.net/v1/disk/public/resources/download?public_key=https://disk.yandex.com.tr/d/sLURXWsHH4gDmw&path=${e.path}`
               )
             )
               .then((res) => res.json())
