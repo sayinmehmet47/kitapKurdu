3584463654b04945a8eb7c4787dc58ba18f049e2
mehmet <sayinmehmet47@gmail.com>
2022-12-11 09:59:22 +0100

    remove unused codes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import { Search } from './components/Search';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { AddNewBook } from './components/AddNewBook';
 import Layout from './components/Layout';
 
 function App() {
   return (
     <Layout>
       <Search />
-      {/* <AddNewBook /> */}
     </Layout>
   );
 }
