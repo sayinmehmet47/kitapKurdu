d0472527da40b9bfd6cad846085b824c75cc7131
mehmet <sayinmehmet47@gmail.com>
2023-01-29 23:43:07 +0100

    feat: dropzone integration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,7 @@
+import React from 'react';
 import { Search } from './components/Search';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from './components/Layout';
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { loadUserThunk } from './redux/authSlice';
 
 function App() {
   return (
--- client/src/pages/UploadNewBook.tsx
+++ client/src/pages/UploadNewBook.tsx
@@ -1,10 +1,9 @@
+import React from 'react';
 import axios from 'axios';
-import React, { useState } from 'react';
-import Uploady, { useItemProgressListener } from '@rpldy/uploady';
-import UploadButton from '@rpldy/upload-button';
+
 import { ToastContainer, toast } from 'react-toastify';
-import { BsFillCloudUploadFill } from 'react-icons/bs';
-import { Line } from 'rc-progress';
+import 'react-dropzone-uploader/dist/styles.css';
+import Dropzone from 'react-dropzone-uploader';
 
 import Layout from '../components/Layout';
 import styled from 'styled-components';
@@ -22,6 +21,7 @@ const Container = styled.div`
 
 export default function UploadNewBook() {
   const handleResponse = (response: any) => {
+    console.log(response);
     const addBook = async (response: {
       original_filename: any;
       bytes: any;
@@ -51,24 +51,16 @@ export default function UploadNewBook() {
   return (
     <Layout>
       <Container>
-        <Uploady
-          isSuccessfulCall={(response) => {
-            return handleResponse(JSON.parse(response.response)) as any;
+        <Dropzone
+          getUploadParams={() => ({
+            url: `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/upload?upload_preset=${UPLOAD_PRESET}`,
+          })}
+          onChangeStatus={({ meta, file, xhr }, status) => {
+            if (status === 'done') {
+              handleResponse(JSON.parse(xhr?.response));
+            }
           }}
-          destination={{
-            url: `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/upload`,
-            params: {
-              upload_preset: UPLOAD_PRESET,
-            },
-          }}
-        >
-          <div>
-            <UploadButton>
-              <BsFillCloudUploadFill size={30} />
-            </UploadButton>
-            <span className="ms-4">UPLOAD A BOOK</span>
-          </div>
-        </Uploady>
+        />
       </Container>
       <ToastContainer />
     </Layout>
