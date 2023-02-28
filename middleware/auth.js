d2f06a86cb5c252dad7947f7fb8467ad068c8892
mehmet <sayinmehmet47@gmail.com>
2023-02-28 13:32:33 +0100

    fix: dependencies

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 module.exports = (req, res, next) => {
-  const token = req.header('Authorization').split(' ')[1];
+  const token = req.header('Authorization')?.split(' ')[1];
 
   // Check for token
   if (!token)
