187230333bbe8f8fee4b25f3e220576aa9fcfa60
mehmet <sayinmehmet47@gmail.com>
2023-03-03 19:07:46 +0100

    feat: cards with download link

--- backend/routes/api/books.js
+++ backend/routes/api/books.js
@@ -106,7 +106,7 @@ router.post('/addNewBook', (req, res) => {
 router.get('/recently-added', (req, res) => {
   Books.find({})
     .sort({ date: -1 })
-    .limit(7)
+    .limit(15)
     .exec((err, data) => {
       if (err) console.log(err);
       res.json(data);
