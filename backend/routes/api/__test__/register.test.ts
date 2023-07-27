b7103710b9d5e9eb5b9a98fbab22fc4a66f5ad3a
mehmet sayin <mehmet.sayin@steiner.ch>
2023-07-27 19:28:58 +0200

    add test for register

---
+++ backend/routes/api/__test__/register.test.ts
@@ -0,0 +1,83 @@
+import request from 'supertest';
+import { app } from '../../../app';
+
+it('returns a 201 on successful register', async () => {
+  return request(app)
+    .post('/api/user/register')
+    .send({
+      username: 'test',
+      password: 'password',
+      email: 'example@gmail.com',
+    })
+    .expect(201);
+});
+
+it('returns a 400 with an invalid username', async () => {
+  return request(app)
+    .post('/api/user/register')
+    .send({
+      username: 'te',
+      email: 'example@gmail.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/user/register')
+    .send({
+      email: 'test@gmail.com',
+      username: 'test',
+      password: 'p',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/user/register')
+    .send({
+      email: 'test@gmail.com',
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/user/register')
+    .send({
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('disallows duplicate emails', async () => {
+  await request(app)
+    .post('/api/user/register')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+      username: 'test',
+    })
+    .expect(201);
+  await request(app)
+    .post('/api/user/register')
+    .send({
+      email: 'test@test',
+      password: 'password',
+      username: 'test',
+    })
+    .expect(400);
+});
+
+it('sets a token succesfully', async () => {
+  const response = await request(app)
+    .post('/api/user/register')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+      username: 'test',
+    })
+    .expect(201);
+  expect(response.body.token).toBeDefined();
+  expect(response.body.user).toBeDefined();
+});
