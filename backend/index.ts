dda68de16ff7f0c9fa1bb9def3d1629b55280365
mehmet sayin <mehmet.sayin@steiner.ch>
2023-07-27 19:58:25 +0200

    chenge the text

--- backend/index.ts
+++ backend/index.ts
@@ -22,7 +22,7 @@ const start = async () => {
     }
   }
   app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server started on port pro` + process.env.PORT || 5000);
+    console.log(`Server started on port` + process.env.PORT || 5000);
   });
 };
 
