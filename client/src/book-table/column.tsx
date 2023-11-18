deccbf3f142d904ec49dff33082efe059e253164
mehmet <sayinmehmet47@gmail.com>
2023-11-18 18:55:19 +0100

    feat: add dropdown to data table

--- client/src/book-table/column.tsx
+++ client/src/book-table/column.tsx
@@ -1,4 +1,14 @@
 import { ColumnDef } from '@tanstack/react-table';
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+} from '../components/ui/dropdown-menu';
+import { Button } from '../components/ui/button';
+import { MoreHorizontal } from 'lucide-react';
+import { DropdownMenuContent } from '../components/ui/dropdown-menu';
+import { DropdownMenuLabel } from '../components/ui/dropdown-menu';
+import { DropdownMenuItem } from '../components/ui/dropdown-menu';
+import { DropdownMenuSeparator } from '../components/ui/dropdown-menu';
 import { Book } from 'src/redux/services/book.api';
 
 export const columns: ColumnDef<Book>[] = [
@@ -13,5 +23,33 @@ export const columns: ColumnDef<Book>[] = [
   {
     accessorKey: 'date',
     header: 'Date',
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
