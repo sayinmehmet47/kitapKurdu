eafa0c4d6e02d78190b850ae52b67f6817f53ef8
mehmet <sayinmehmet47@gmail.com>
2024-03-30 00:43:33 +0100

    Update CORS origin in app.ts

--- backend/app.ts
+++ backend/app.ts
@@ -20,8 +20,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
+    origin: ['http://localhost:3000', 'https://www.kitapkurdu.xyz'],
   })
 );
 
