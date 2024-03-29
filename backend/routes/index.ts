a22d37253ec46c9396330af925618aca2bfdf572
mehmet <sayinmehmet47@gmail.com>
2024-03-30 00:53:56 +0100

    Add subscription router to routes

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { booksRouter } from './api/books';
 import { userRouter } from './api/user';
 import { messagesRouter } from './api/messages';
+import { subscriptionRouter } from './api/subscription';
 
 const router = express.Router();
 
 router.use('/books', booksRouter);
 router.use('/user', userRouter);
 router.use('/messages', messagesRouter);
+router.use('/subscription', subscriptionRouter);
 
 export default router;
