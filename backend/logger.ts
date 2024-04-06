c87a580c1b89b94539e1201944605a0de4f626ad
Mehmet Sayin <mehmetsayin@Mehmets-MacBook-Pro.local>
2024-04-06 13:21:18 +0200

    feat: timestamp added for error

--- backend/logger.ts
+++ backend/logger.ts
@@ -2,9 +2,14 @@ import winston from "winston";
 
 export const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.json(),
+    format: winston.format.combine(
+        winston.format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        winston.format.json()
+    ),
     transports: [
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' }),
+        new winston.transports.File({ filename: 'error.log', level: 'error'}),
+        new winston.transports.File({ filename: 'combined.log' }),
     ],
-  });
+});
