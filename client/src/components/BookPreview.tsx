0a362da68a20d81cbe7eca34ddc535168046c3a9
mehmet <sayinmehmet47@gmail.com>
2024-10-25 14:59:01 +0200

    fix: use google pdf preview to preview pdf

--- client/src/components/BookPreview.tsx
+++ client/src/components/BookPreview.tsx
@@ -1,53 +1,53 @@
-import { ArrowLeftIcon } from 'lucide-react';
-import { useState } from 'react';
-import { ReactReader } from 'react-reader';
-import { useNavigate } from 'react-router-dom';
+import {ArrowLeftIcon} from 'lucide-react';
+import {useState} from 'react';
+import {ReactReader} from 'react-reader';
+import {useNavigate} from 'react-router-dom';
 
 interface BookPreviewProps {
-  bookUrl: string;
-  fileType: string;
-  bookName: string;
+    bookUrl: string;
+    fileType: string;
+    bookName: string;
 }
 
 export const BookPreview = ({
-  bookUrl,
-  fileType,
-  bookName,
-}: BookPreviewProps) => {
-  const [location, setLocation] = useState<string | number>(0);
-  const navigate = useNavigate();
+                                bookUrl,
+                                fileType,
+                                bookName,
+                            }: BookPreviewProps) => {
+    const [location, setLocation] = useState<string | number>(0);
+    const navigate = useNavigate();
 
-  if (fileType === 'pdf') {
-    return (
-      <>
-        <ArrowLeftIcon
-          size={32}
-          className="cursor-pointer border border-gray-400 rounded-full hover:bg-gray-200 hover:shadow-md relative left-2 my-2"
-          onClick={() => navigate(-1)}
-        />
-        <embed
-          src={bookUrl}
-          type="application/pdf"
-          className="w-full h-[calc(100vh-44px)]"
-        />
-      </>
-    );
-  } else if (fileType === 'epub') {
-    return (
-      <div className="h-screen">
-        <ArrowLeftIcon
-          size={32}
-          className="cursor-pointer border border-gray-400 rounded-full hover:bg-gray-200 hover:shadow-md relative left-2  my-2"
-          onClick={() => navigate(-1)}
-        />
-        <ReactReader
-          url={bookUrl}
-          location={location}
-          locationChanged={(epubcfi: string) => setLocation(epubcfi)}
-          title={bookName}
-          showToc={true}
-        />
-      </div>
-    );
-  }
+    if (fileType === 'pdf') {
+        return (
+            <>
+                <ArrowLeftIcon
+                    size={32}
+                    className="cursor-pointer border border-gray-400 rounded-full hover:bg-gray-200 hover:shadow-md relative left-2 my-2"
+                    onClick={() => navigate(-1)}
+                />
+                <iframe
+                    src={`https://drive.google.com/viewerng/viewer?embedded=true&url=${encodeURIComponent(bookUrl)}`}
+                    className="w-full h-[calc(100vh-44px)]"
+                    frameBorder="0"
+                />
+            </>
+        );
+    } else if (fileType === 'epub') {
+        return (
+            <div className="h-screen">
+                <ArrowLeftIcon
+                    size={32}
+                    className="cursor-pointer border border-gray-400 rounded-full hover:bg-gray-200 hover:shadow-md relative left-2  my-2"
+                    onClick={() => navigate(-1)}
+                />
+                <ReactReader
+                    url={bookUrl}
+                    location={location}
+                    locationChanged={(epubcfi: string) => setLocation(epubcfi)}
+                    title={bookName}
+                    showToc={true}
+                />
+            </div>
+        );
+    }
 };
